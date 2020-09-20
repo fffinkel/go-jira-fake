7e374e35823e2b1f976d9090f8bd3cbc9f6369b8
Matt Finkel <finkel.matt@gmail.com>
2020-09-20 17:16:51 +0000

    Add issue transition

--- main.go
+++ main.go
@@ -10,15 +10,35 @@ import (
 func main() {
 	fmt.Println("go-jira-fake: My fake project that uses go-jira")
 
+	testIssueID := "FART-1"
+
 	tp := jira.BasicAuthTransport{
 		Username: "finkel.matt@gmail.com",
 		Password: os.Getenv("_FAKE_"),
 	}
 
 	jiraClient, _ := jira.NewClient(tp.Client(), "https://go-jira-finkel.atlassian.net/")
-	issue, _, _ := jiraClient.Issue.Get("FART-1", nil)
+
+	issueService := jiraClient.Issue
+	issue, _, _ := issueService.Get(testIssueID, nil)
 
 	fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
 	fmt.Printf("Type: %s\n", issue.Fields.Type.Name)
 	fmt.Printf("Priority: %s\n", issue.Fields.Priority.Name)
+	fmt.Printf("Status: %s\n", issue.Fields.Status.Name)
+
+	possibleTransitions, _, _ := issueService.GetTransitions("FART-1")
+	currentStatus := issue.Fields.Status.Name
+	var transitionID string
+	for _, v := range possibleTransitions {
+		if v.Name != currentStatus {
+			transitionID = v.ID
+			break
+		}
+	}
+
+	issueService.DoTransition("FART-1", transitionID)
+	issue, _, _ = issueService.Get(testIssueID, nil)
+
+	fmt.Printf("Status after transition: %+v\n", issue.Fields.Status.Name)
 }
